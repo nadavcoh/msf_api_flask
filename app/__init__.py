13bff85735994cfb1ddacffa70c99a156fd9cde4
nadavcoh <cohen.n@gmail.com>
2023-03-11 22:55:14 +0200

    Config location

--- app/__init__.py
+++ app/__init__.py
@@ -86,7 +86,7 @@ def create_app(test_config=None):
     )
     
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=False)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'msf_api_flask.sqlite'),
@@ -97,7 +97,7 @@ def create_app(test_config=None):
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile('../config.py', silent=False)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
