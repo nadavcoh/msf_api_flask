156297dcccb91683fc0b5d41e3254481eda449ce
nadavcoh <cohen.n@gmail.com>
2023-03-12 21:46:32 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -97,7 +97,7 @@ def create_app(test_config=None):
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('/etc/secrets/config.py', silent=False)
+        app.config.from_pyfile('/etc/secrets/config.py', silent=True)
         app.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
