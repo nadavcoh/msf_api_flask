88f97a02f363c161895284244697d69188fe841b
nadavcoh <cohen.n@gmail.com>
2023-10-13 15:02:39 +0300

    redis-server

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-open -a iTerm.app "/usr/local/bin/redis-stack-server"
+open -a iTerm.app "/usr/local/bin/redis-server"
 . venv/bin/activate
 flask --debug run --port=2705 --cert=key.crt --key=key.key
