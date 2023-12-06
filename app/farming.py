44c1112746cb4f16df011f9898e95b1f298a2b2b
nadavcoh <cohen.n@gmail.com>
2023-12-06 23:19:17 +0200

    Update farming.py

--- app/farming.py
+++ app/farming.py
@@ -199,7 +199,7 @@ def get_farming_table_html_rs():
     farming = get_farming_inventory()
     farming.drop(columns="locations", inplace = True)
     farming = farming.loc[(farming["explode1"]=="RS")]
-    farming.sort_values(["red", "red_delta"], ascending=[False, Fales] , inplace=True)
+    farming.sort_values(["red", "red_delta"], ascending=[False, False] , inplace=True)
     farming.drop(columns = ["explode1", "explode2", "explode3", "explode4"], inplace=True)
     return farming.style.format(thousands=",",
                                 formatter={'icon': lambda x: "<img class='reward_icon' src='{}'>".format(x),
